ca10029ee08730dd85821e6032ef064ecdf623f4
Grant Chan <grant.r.chan@gmail.com>
2020-01-07 21:55:52 -0600

    completed candle chart

--- python_stocks_analysis.py
+++ python_stocks_analysis.py
@@ -9,8 +9,8 @@ from mpl_finance import candlestick_ohlc
 #set minor ticks on other days, major on Mondays
 from matplotlib.dates import DateFormatter,date2num, WeekdayLocator, DayLocator, MONDAY
 
-start_date = datetime.datetime(2012,1,1)
-end_date = datetime.datetime(2020, 1, 1)
+start_date = datetime.datetime(2012, 1, 1)
+end_date = datetime.datetime(2018, 3, 1)
 
 def stock_printer(start,end,ticker):
     return pdr.data.DataReader(ticker, 'quandl', start.date(), end.date(), api_key=quandl_api_key)
@@ -22,13 +22,16 @@ def moving_average(df, days):
     return df['Open'].rolling(days).mean()
 
 tesla_stock = stock_printer(start_date, end_date, 'TSLA')
+# print(tesla_stock)
 tesla_stock['Total Traded'] = total_traded(tesla_stock)
 tesla_stock['MA50'] = moving_average(tesla_stock, 50)
 tesla_stock['MA200'] = moving_average(tesla_stock, 100)
 tesla_reset = tesla_stock.loc['2018-01'].reset_index()
 # print(tesla_reset.info())
 tesla_reset['date_ax'] = tesla_reset['Date'].apply(lambda date: date2num(date)) #create numerical date column
-tesla_values = [tuple(vals) for vals in tesla_reset[['date_ax','Open','High','Low','Close']]]
+tesla_values = [tuple(vals) for vals in tesla_reset[[
+    'date_ax', 'Open', 'High', 'Low', 'Close']].values]
+# print(tesla_values)
 
 ford_stock = stock_printer(start_date, end_date, 'F')
 ford_stock['Total Traded'] = total_traded(ford_stock)
@@ -46,6 +49,7 @@ car_companies.columns = ['Tesla Open', 'Ford Open', 'GM Open']
 
 ''' Show the plots, using plot function in pandas-datareader '''
 fig, axes = plt.subplots(nrows=2,ncols=2)
+fig.subplots_adjust(bottom=0.2)
 plt1 = tesla_stock[['Open', 'MA50', 'MA200']].plot(figsize=(12, 8), title='Opening Prices', ax=axes[0, 0])
 ford_stock['Open'].plot(ax=axes[0,0])
 gm_stock['Open'].plot(ax=axes[0,0])
@@ -72,9 +76,18 @@ alldays = DayLocator() # minor ticks on every other day beside Mondays
 week_formatter = DateFormatter('%b %d') # format e.g., Jan 12
 day_formatter = DateFormatter('%b %d') # e.g., 12
 
+'''Set candle chart as plot #4'''
+plt4_axes = axes[1, 1]
+plt4_axes.xaxis.set_major_locator(mondays)
+plt4_axes.xaxis.set_minor_locator(alldays)
+plt4_axes.xaxis.set_major_formatter(week_formatter)
+plt4_axes.set_xlabel('Date')
+plt4_axes.set_ylabel('US Dollars/share')
+candlestick_ohlc(plt4_axes,tesla_values,width=0.6,colorup='g',colordown='r')
+
 plt.tight_layout()
 
-plt4 = scatter_matrix(car_companies, figsize=(8,8), alpha=0.2, hist_kwds={'bins': 50})
+plt5 = scatter_matrix(car_companies, figsize=(8,8), alpha=0.2, hist_kwds={'bins': 50}) #use alpha to adjust data point transparency
 
 plt.show()
 
